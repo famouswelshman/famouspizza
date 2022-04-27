104d97b6d1fe0bfd78e7c7b118301180d825c644
James Hunt <famouswelshman@yahoo.co.uk>
2022-04-27 16:38:57 +0000

    added home page content

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='home')
 ]
-
