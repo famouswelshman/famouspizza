a2d6688097e18ac519db728fbde37326e94a77c9
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-02 16:28:18 +0000

    css updated

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -6,6 +6,7 @@ from .models import Product
 
 
 def all_products(request):
+    """ Page to display all products with search function """
 
     products = Product.objects.all()
 
@@ -14,3 +15,15 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """ Page to djsplay full details of one specific product with search function """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
