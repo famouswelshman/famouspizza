bcc62e3d9e36cb52f4a50a98a04e34c34703def5
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-03 10:24:32 +0000

    added search functionality

--- products/models.py
+++ products/models.py
@@ -22,6 +22,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    image_2 = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Product
 
 
@@ -9,9 +11,21 @@ def all_products(request):
     """ Page to display all products with search function """
 
     products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria was entered!!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
