39df0f7b44664274a2e46fb092c55115dc35da95
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-03 12:46:45 +0000

    added images to products

--- products/views.py
+++ products/views.py
@@ -11,6 +11,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
         if 'category' in request.GET:
@@ -31,6 +32,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
