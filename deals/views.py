b032f039c81424fe61f72a0c83745c8f353c8299
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-08 11:45:21 +0000

    deals detail.html added

--- deals/views.py
+++ deals/views.py
@@ -12,3 +12,14 @@ def deals(request):
     }
 
     return render(request, 'deals/deals.html', context)
+
+def deal_detail(reques):
+    """ Page to djsplay full details of one specific product with search function """
+
+    deal = get_object_or_404(Deal, pk=deal_id)
+
+    context = {
+        'deal': deal,
+    }
+
+    return render(request, 'products/product_detail.html', context)
