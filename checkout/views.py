6ff10cff6256852093de18ff0fd3b2b89c136917
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-12 09:17:30 +0000

    checkout link added to cart

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
@@ -6,10 +6,11 @@ from .forms import OrderForm
 
 # Create your views here.
 def checkout(request):
-    cart = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "There is nothing in your shopping cart")
         return redirect(reverse('products'))
+        print("line 13")
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
