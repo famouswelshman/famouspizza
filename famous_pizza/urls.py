3c949d6b36a4c8d3ab7247b849aaa7d6666b7fdb
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-12 08:59:43 +0000

    form added to checkout

--- famous_pizza/urls.py
+++ famous_pizza/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('cart/', include('cart.urls')),
     path('deals/', include('deals.urls')),
+    path('checkout/', include('checkout.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
