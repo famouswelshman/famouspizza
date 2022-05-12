600a9f73df9610c7682abfcfda979a44b1db7a0e
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-12 15:19:29 +0000

    Updated checkout with stripe secret key

--- checkout/views.py
+++ checkout/views.py
@@ -10,12 +10,13 @@ def checkout(request):
     if not cart:
         messages.error(request, "There is nothing in your shopping cart")
         return redirect(reverse('products'))
-        print("line 13")
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KycscFC5DY9G8LxLFtY5EEwxdz1Nzup83RAQ70NgjgYNzEwlKjVuEbzpPqIkwvHQPCA2MA4HaYAhV6ZYAEIeRST0084k2ujKw',
+        'client_secret': 'test_client_secret',
     }
 
     return render(request, template, context)
