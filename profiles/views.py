5c0f84b3d08cd05eee7511edef8241803f5e2156
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-19 08:33:49 +0000

    profile tremplate css updated

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def profile(request):
     """ Displays user profile """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
-    context = {}
-
+    context = {
+        'form': form,
+        'orers': orders,
+    }
 
     return render(request, template, context)
