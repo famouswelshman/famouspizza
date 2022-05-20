81875fbcf8a08c9129444cc43821786ef858c7c7
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-20 11:10:07 +0000

    product admin page added with fields

--- products/models.py
+++ products/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    def get_friendly_name(self):
+        return self.friendly_name
+
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
