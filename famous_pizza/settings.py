bd29bf1bc3be39da87508ed4baa144090702a4fe
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-21 11:05:58 +0000

    updated settings

--- famous_pizza/settings.py
+++ famous_pizza/settings.py
@@ -125,7 +125,7 @@ WSGI_APPLICATION = 'famous_pizza.wsgi.application'
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_urk.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
 else:
     DATABASES = {
