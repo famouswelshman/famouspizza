3de1fcb6c704414e0edbbc48463f5561a8efe8df
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-22 16:34:32 +0000

    settings updated

--- famous_pizza/settings.py
+++ famous_pizza/settings.py
@@ -122,7 +122,7 @@ WSGI_APPLICATION = 'famous_pizza.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
@@ -132,7 +132,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
