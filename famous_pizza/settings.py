fe5e98766c97b2f6e97fb4e5e5dc4eb9b41171a6
James Hunt <famouswelshman@yahoo.co.uk>
2022-05-22 16:49:55 +0000

    secret key added

--- famous_pizza/settings.py
+++ famous_pizza/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4h-p=zi(ka($q=k2q3*rzheecgnp8)8ua#4d!@u^fb#r8e443p'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['famous-pizza.herokuapp.com', 'localhost']
 
